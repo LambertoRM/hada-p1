f48486ff3dcfe08059b1d1683a8b365aacc6d446
Lamberto Ruiz Martínez <lrm76@mscloud.ua.es>
2019-02-19 12:27:48 +0100

    Commented HadaP1 class

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -5,10 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace hada_p1
-{
+{   
+    /// <summary>
+    /// Esta clase contiene métodos para convertir de sedundos a minutos y viceversa
+    /// </summary>
     class HadaP1
     {
-         public static double Seconds2Minutes(double s)
+        /// <summary>
+        /// Seconds2Minutes is a method in the HadaP1 class.
+        /// </summary>
+        /// <param name="s">seconds</param>
+        /// <returns>Returns minutes.</returns>
+        public static double Seconds2Minutes(double s)
         {
             if(s == 0)
             {
@@ -19,6 +27,11 @@ namespace hada_p1
             }
         }
 
+        /// <summary>
+        /// Minutes2Seconds is a method in the HadaP1 class.
+        /// </summary>
+        /// <param name="m">minutes</param>
+        /// <returns>Returns seconds.</returns>
         public static double Minutes2Seconds(double m)
         {
             return m * 60;
